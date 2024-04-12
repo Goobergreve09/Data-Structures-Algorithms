a4a6a226978e65d26acfac93b25e3e6d90751dce
Goobergreve09 <gregory.greve@yahoo.com>
2024-04-12 01:41:04 -0400

    added objects for cleaner code re-use

--- script.js
+++ script.js
@@ -19,18 +19,50 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+    {
+        name: "town square",
+        text: "You are in the town square. You see a sign that says \"Store\".",
+        "button text": [
+            "Go to store",
+            "Go to cave",
+            "Fight dragon"
+        ],
+        "button functions": [
+            goStore,
+            goCave,
+            fightDragon
+        ]
+    },
+
+    {
+        name: "store",
+        text: "You enter the store.",
+        "button text": [
+            "Buy 10 health (10 gold)",
+            "Buy weapon (30 gold)",
+            "Go to town square"
+        ],
+        "button functions": [
+            buyHealth,
+            buyWeapon,
+            goTown
+        ]
+    },
+
+
+];
+
+
+
 // initialize buttons
 
+function goTown() {
+    update(locations[0])
+}
+
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-
-  text.innerText = "You enter the store.";
-
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
+    update(locations[1])
 }
 
 function goCave() {
@@ -45,7 +77,21 @@ function buyHealth() {}
 
 function buyWeapon() {}
 
-function goTown() {}
+function fightSlime() {}
+
+function fightBeast() {}
+
+function update(location){
+    button1.innerText = location["button text"][0];
+    button2.innerText = location ["button text"][1];
+    button3.innerText = location ["button text"][2];
+  
+    text.innerText = location.text;
+  
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+}
 
 button1.onclick = goStore;
 button2.onclick = goCave;
