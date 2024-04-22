a61e1720befcb16a9e6200d30c9959cf4195b774
Goobergreve09 <gregory.greve@yahoo.com>
2024-04-22 10:52:04 -0400

    sell weapon function completed

--- script.js
+++ script.js
@@ -119,6 +119,20 @@ function fightSlime() {}
 
 function fightBeast() {}
 
+function sellWeapon () {
+  if (inventory.length > 1){
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon;
+    currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+
+  } else {
+    text.innerText = "Don't sell your only weapon!"
+  }
+}
+
 function update(location) {
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
